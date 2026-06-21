da52fc52fa2633af896732f9efcb455376fc694d
Ray Ray <ericlee20030829@gmail.com>
2026-06-21 20:06:22 +0800

    First commit

---
+++ src/main/java/com/farmily/Cka101G3Application.java
@@ -0,0 +1,13 @@
+package com.farmily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cka101G3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cka101G3Application.class, args);
+	}
+
+}
