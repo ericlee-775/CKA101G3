a26e97c4cf783c88495b80a62a7ff2bf648cef28
Ray Ray <ericlee20030829@gmail.com>
2026-07-07 02:07:58 +0800

    購物車insert CRUD

--- src/main/java/com/farmily/shoppingcart/service/ProductShoppingCartService.java
+++ src/main/java/com/farmily/shoppingcart/service/ProductShoppingCartService.java
@@ -1,5 +1,17 @@
 package com.farmily.shoppingcart.service;
 
-public interface ProductShoppingCartService {
+import java.util.List;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.farmily.shoppingcart.dto.ShoppingcartDTO;
+import com.farmily.user.security.MemberUserDetails;
+
+public interface ProductShoppingCartService {
+	List<ShoppingcartDTO> getcard(Integer userId);
+	void addToCart(Integer productId,Integer quantity,Integer userId);
+	void updatedQuantity(Integer productId,Integer quantity,Integer userId);
+	void deleteCart(Integer userId,Integer productId);
 }
